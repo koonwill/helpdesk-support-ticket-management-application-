16aff35fbc5dc0a7ec88a4251dd414549e63fb35
koonwill <93331101+koonwill@users.noreply.github.com>
2024-04-18 02:56:10 +0700

    remove something

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login';
-import Menu from './pages/Menu';
-import Order from './pages/Order';
-import Promotions from './pages/Promotions';
-import Data from './pages/Data';
-import Logout from './pages/Logout';
 import Tickets from './pages/Tickets';
 
 
@@ -13,13 +7,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />}/>
           <Route path="/tickets" element={<Tickets />}/>
-          <Route path="/menu" element={<Menu/>}/>
-          <Route path="/order" element={<Order />}/>
-          <Route path="/promotions" element={<Promotions />}/>
-          <Route path="/data" element={<Data />}/>
-          <Route path="/logout" element={<Logout />}/>
         </Routes>
       </BrowserRouter>
     </>
