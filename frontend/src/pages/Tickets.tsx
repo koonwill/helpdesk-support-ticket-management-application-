43e365f2b84a8b4f33e3dfbda88ef4332bb1d315
koonwill <93331101+koonwill@users.noreply.github.com>
2024-04-18 06:54:12 +0700

    fix tailwind

--- frontend/src/pages/Tickets.tsx
+++ frontend/src/pages/Tickets.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Tickets.css';
 
 function Tickets() {
     const [tickets, setTickets] = useState([]);
@@ -18,11 +17,22 @@ function Tickets() {
         fetchTickets();
     }, []);
 
-    const onDragEnd = (result) => {};
+    const onDragEnd = (result) => { };
 
 
     return (
-        <div className="tickets-container">
+        <div
+            className="
+          m-auto
+          flex
+          min-h-screen
+          w-full
+          items-center
+          overflow-x-auto
+          overflow-y-hidden
+          px-[40px]
+      "
+        >
             <h1 className="tickets-title">Tickets</h1>
             <div className="tickets">
                 {tickets.map(ticket => (
@@ -37,6 +47,9 @@ function Tickets() {
                         {/* Add more ticket details here */}
                     </div>
                 ))}
+            </div>
+            <div className="add-tickets-bttn">
+                <button className='' justify-center>Add Ticket</button>
             </div>
         </div>
     );
